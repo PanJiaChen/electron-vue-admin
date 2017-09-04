d9891acde2392046fb558815b40c3ef70e47f7ef
tingrun919 <344197917@qq.com>
2017-09-04 15:09:06 +0800

    修改npm install时的错误

--- app/src/main/index.dev.js
+++ app/src/main/index.dev.js
@@ -20,9 +20,9 @@ require('electron-debug')({ showDevTools: true })
 
 // Install `vue-devtools`
 require('electron').app.on('ready', () => {
-  let installExtension = require('electron-devtools-installer')
+  const installExtension = require('electron-devtools-installer')
   installExtension.default(installExtension.VUEJS_DEVTOOLS)
-    .then(() => {})
+    .then(() => ({}))
     .catch(err => {
       console.log('Unable to install `vue-devtools`: \n', err)
     })
